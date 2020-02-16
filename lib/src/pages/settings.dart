90240328963a7735380f86551d2f5cf52b283383
fulton <gprlabs@outlook.com>
2020-02-16 15:43:42 -0800

    working on adding some proper stocks

---
+++ lib/src/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
