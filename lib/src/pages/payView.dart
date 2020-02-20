3b5c5012f1e51400a8a6656dbf71d3212e5d2d78
fulton <gprlabs@outlook.com>
2020-02-19 16:34:17 -0800

    working on the tabs

---
+++ lib/src/pages/payView.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PayView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
--- lib/src/util/getStocks.dart
+++ lib/src/util/getStocks.dart
@@ -1,7 +1,11 @@
 import 'package:solar_flare/src/models/stocks.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 List<Stocks> getStocks({String request}){
   var stocks = new List<Stocks>();
+  final HttpLink httpLink = HttpLink(
+    uri: 'https://api.github.com/graphql',
+  );
   stocks.add(Stocks(name: "test", graph: "assets/graphs/bitcoin_price_graph.png", growth: 1.0));
   return stocks;
 }
