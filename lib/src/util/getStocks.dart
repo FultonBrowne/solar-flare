ca8b544e92bac262ec83ac7930c40da64f6a3edf
fulton <gprlabs@outlook.com>
2020-03-12 15:52:07 -0700

    adding support for the api

--- lib/src/util/getStocks.dart
+++ lib/src/util/getStocks.dart
@@ -1,11 +1,20 @@
 import 'package:solar_flare/src/models/stocks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-List<Stocks> getStocks({String request}){
-  var stocks = new List<Stocks>();
-  final HttpLink httpLink = HttpLink(
-    uri: 'https://api.github.com/graphql',
+
+ getStocks(String sym) async {
+  HttpLink link = HttpLink(uri: "to do"); // you can also use headers for authorization etc.
+  GraphQLClient client = GraphQLClient(link: link, cache: InMemoryCache());
+
+  QueryOptions query = QueryOptions(
+      document: "query getSingleCourse(\$courseID: String!) \{" +
+      "course(id: \$courseID) {"
+    "sym\n"
+    "price\n"
+  "\}"
+,
+variables: {'id' : sym}
   );
-  stocks.add(Stocks(name: "test", graph: "assets/graphs/bitcoin_price_graph.png", growth: 1.0));
-  return stocks;
+
+  var result = await client.query(query);
 }
